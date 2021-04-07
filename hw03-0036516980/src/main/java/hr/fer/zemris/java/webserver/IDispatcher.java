bf80d64c4b5dec20747074791c8daf13b7456426
Vedran Hernaus <vedran.hernaus@fer.hr>
2021-04-07 22:32:59 +0200

    rijesen lexer

--- hw03-0036516980/src/main/java/hr/fer/zemris/java/webserver/IDispatcher.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.zemris.java.webserver;
-
-public interface IDispatcher {
-	void dispatchRequest(String urlPath) throws Exception;
-}
--- hw03-0036516980/src/main/java/hr/fer/zemris/java/webserver/IWebWorker.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.zemris.java.webserver;
-
-public interface IWebWorker {
-	public void processRequest(RequestContext context) throws Exception;
-}
